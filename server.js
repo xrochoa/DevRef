7bc6161345724b9eafa2c2ded7eb7ee993ae3c0a
Xavier Reyes Ochoa <x.a.reyes-ochoa@hotmail.com>
2016-06-08 00:12:20 -0700

    updated css styles and data

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var server = new express();
+var path = require('path');
+
+
+server.use(express.static(__dirname + '/dist'));
+server.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 server.listen(8000, function() {
-    server.use(express.static(__dirname + '/dist'));
     console.log('Serving at http://localhost:8000');
 });
